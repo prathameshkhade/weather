a8e1746a14088923b2c66fee42aed4d4f8d36399
Prathamesh Khade <pkhade2865@gmail.com>
2024-06-18 11:50:52 +0000

    add: search bar in nav

--- weather/src/pages/Navbar.jsx
+++ weather/src/pages/Navbar.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar() {
   return (
-    <div className="flex py-2 justify-between px-6 bg-gray-200 font-semibold drop-shadow-xl">
+    <div className="flex align-items-center py-2 justify-between px-6 bg-gray-200 font-semibold drop-shadow-lg">
       <div className="text-2xl">Weather</div>
-      <div className="">{/* search bar component */}</div>
+      <div className="">
+        <Search />
+      </div>
       <div className="gap-4">
         <nav className="space-x-5">
-          <a href="#">About</a>
+          <a href="#.">About</a>
         </nav>
       </div>
     </div>
   );
+}
+
+// Search bar component
+function Search() {
+  let [location, setLocation] = useState('')
+
+  useEffect((e) => {
+    // logic of search
+  }, [location])
+
+  return (
+    <div className="">
+      <input type="text" placeholder="Search" value={location} onChange={(event) => setLocation(event.target.value)} 
+          className="rounded-2xl px-2 w-[250px] h-[30px] text-center text-black"
+      />
+    </div>
+  )
 }
